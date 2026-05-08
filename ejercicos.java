b2d1dd7bd7a15ddc0b561f2d8b564f17bd5bad97
alma <almaluzmaldonado4@gmail.com>
2026-05-07 21:43:59 -0300

    Create ejercicos.java

---
+++ ejercicos.java
@@ -0,0 +1,98 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+ 
+        //1
+   // for (int i = 1; i < 11; i++) {
+     // System.out.println(i);
+   // }
+
+    //2
+    //for (int Contador = 0; Contador < 21; Contador++) {
+     //  if (Contador % 2 == 0) {
+  //System.out.println(Contador);
+     //   }
+   // }
+
+   //3
+   //int cant = 0;
+   //for (int Contador = 0; Contador < 101; Contador++) {
+     //cant += Contador;
+     
+   //}
+   //System.out.println(cant);
+   
+   //while 1
+   
+   //4
+   //int i = 5;
+        //while (i >= 0){
+            //System.out.println(i);
+            //i--;
+        //}
+        //System.out.println("¡Despegue!");
+   
+   //5
+   //int realnumero= 23;
+   //System.out.print("ingrese un numero del 1 al 50: ");
+   //int numero = sc.nextInt();
+   //while (numero != realnumero) {
+     //System.out.println("Incorrecto, ingrese otro numero: ");
+     //numero = sc.nextInt();
+   //}
+   
+   //System.out.println("correcto, era: "+ realnumero);
+  
+  //6
+  //String usuario = "Alma";
+        //String Contrasena = "1234";
+        //Boolean correcta = false;
+        //String intento = "";
+        //while (!correcta){
+            //System.out.print(usuario + ", Ingrese su contraseña: ");
+            //intento = sc.nextLine(); 
+            //if (!intento.equals(Contrasena)) System.out.print("Incorrecto ");
+            //else correcta = true; 
+        //}
+        //System.out.print("Contraseña correcta");
+  
+  
+  // do while
+  
+  //1
+ //int Respuesta = 0;
+    //do {
+       //System.out.println( "*******MENU******* \nGaseosa*******100$ \nPan***********300$ \nAgua***********50$");
+       //System.out.print("Quieres volver a ver el menu??(si= 1, no=2): ");
+       //Respuesta = sc.nextInt();
+     //} while (Respuesta != 2);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   sc.close();
+  }    
+}
+    
+
+
+
